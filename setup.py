fa79fee9a6ecf7e884b78488e60491f153c9a49b
SandeepAroha07 <54171260+SandeepAroha07@users.noreply.github.com>
2025-01-15 12:42:28 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+import time
+import os
+
+setup(name='o9testcodeAA',
+      packages=['o9dummyfolder_Sandeep'],
+      include_package_data=True
+      )
