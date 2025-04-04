8ac8a8b3e1474bd5f2018257c3ec95993e78cd65
SandeepAroha07 <54171260+SandeepAroha07@users.noreply.github.com>
2025-04-04 08:50:37 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import time
 import os
 
-setup(name='o9testcodeAA',
-      packages=['o9dummyfolder_Sandeep'],
+setup(name='o9testcodeAA_v1',
+      packages=['o9dummyfolder_Sandeep_v1'],
       include_package_data=True
       )
